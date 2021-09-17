8cb789139bf5f604230e0c924b75ca46774632ca
Melvin Narayan <80097858+Melvin-n@users.noreply.github.com>
2021-09-18 02:24:07 +1200

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,53 @@ let oppPaddleHeight = 50;
 let oppPaddleWidth = 10;
 let ballRadius = 10;
 
-//draw function which will as per framerate
+//movement variables
+let playerPaddleY = 135;
+let oppPaddleY = 135;
+//ball movement vars and iterators
+let x = 240;
+let y = 160;
+let dx = -2;
+let dy = 2;
+
+//movement flags
+let pressDown = false;
+let pressUp = false;
+
+
+
+//main draw function which will refresh per framerate
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle();
+    drawOppPaddle();
+    drawBall();
+
+    //check for kep press flags, move player paddle accordingly
+    if(pressDown == true){
+        playerPaddleY += 4;
+        if (playerPaddleY > canvas.height - playerPaddleHeight){
+            playerPaddleY = canvas.height - playerPaddleHeight
+        }
+    } else if(pressUp == true) {
+        playerPaddleY -= 4;
+        if(playerPaddleY < 0 ){
+            playerPaddleY  = 0 ;
+        }
+    }
+
+
+}
+
+
+//refresh rate every 10ms
+setInterval(draw, 10);
+
+
+//sub draw functions 
 function drawPaddle(){
     ctx.beginPath();
-    ctx.rect(10, 135, playerPaddleWidth, playerPaddleHeight);
+    ctx.rect(10, playerPaddleY, playerPaddleWidth, playerPaddleHeight);
     ctx.fillStyle = 'black';
     ctx.fill()
     ctx.closePath();
@@ -35,6 +78,26 @@ function drawBall(){
 
 }
 
-drawPaddle();
-drawOppPaddle();
-drawBall();
+
+//event handlers for key up and down
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+//event handler functions using boolean flags
+function keyUpHandler(e){
+    if(e.key == 'ArrowUp' || e.key == 'w'){
+        pressUp = false;
+    } else if (e.key == 'ArrowDown' || e.key == 's') {
+        pressDown = false;
+    }
+}
+
+function keyDownHandler(e){
+    if(e.key == 'ArrowUp' || e.key == 'w'){
+        pressUp = true;
+    } else if (e.key == 'ArrowDown' || e.key == 's') {
+        pressDown = true;
+    }
+}
+
+draw();
