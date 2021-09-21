a2c7e3d5b81afb6c80d3aac9c0525672fcc437a0
Melvin Narayan <80097858+Melvin-n@users.noreply.github.com>
2021-09-22 01:35:11 +1200

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let x = 240;
 let y = 160;
 let dx = 3;
 let dy = 2;
-console.log(dx)
+console.log
 //movement flags
 let pressDown = false;
 let pressUp = false;
@@ -34,59 +34,78 @@ let start = false;
 let rally =  document.getElementById('rally');
 let rallyCount = 0;
 
+//win/lose flags
+let winState = false;
+let loseState = false;
+
 
 //main draw function which will refresh per framerate
 function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPaddle();
-    drawOppPaddle();
-    drawBall();
-    rally.innerHTML = 'Rally: ' + rallyCount;
 
+    if(winState == false && loseState == false){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawPaddle();
+        drawOppPaddle();
+        drawBall();
+        rally.innerHTML = 'Rally: ' + rallyCount;
 
-    //check for key press flags, move player paddle accordingly
-    if(pressDown == true){
-        playerPaddleY += 4;        
-        if (playerPaddleY > canvas.height - playerPaddleHeight){
-            playerPaddleY = canvas.height - playerPaddleHeight
+        //check for key press flags, move player paddle accordingly
+        if(pressDown == true){
+            playerPaddleY += 4;        
+            if (playerPaddleY > canvas.height - playerPaddleHeight){
+                playerPaddleY = canvas.height - playerPaddleHeight
+            }
+        } else if(pressUp == true) {
+            playerPaddleY -= 4;
+            if(playerPaddleY < 0 ){
+                playerPaddleY  = 0 ;
+            }
         }
-    } else if(pressUp == true) {
-        playerPaddleY -= 4;
-        if(playerPaddleY < 0 ){
-            playerPaddleY  = 0 ;
-        }
-    }
 
-    if(start == false){
-        ballY = playerPaddleY + playerPaddleHeight / 2;
-        ballX = playerPaddleX + playerPaddleWidth + ballRadius / 2;
+        if(start == false){
+            ballY = playerPaddleY + playerPaddleHeight / 2;
+            ballX = playerPaddleX + playerPaddleWidth + ballRadius / 2;
+            
+        }
+
+        if(start == true){
+            ballX += dx;
+            ballY -= dy;
+
         
-    }
-
-    if(start == true){
-        ballX += dx;
-        ballY -= dy;
-
+            if(ballX < playerPaddleX + playerPaddleWidth && ballY > playerPaddleY  && ballY < playerPaddleY + playerPaddleHeight){
+                dx = -dx
+                rallyCount ++;
+            }
+            else if(ballX == oppPaddleX && ballY > oppPaddleY - oppPaddleHeight / 2 && ballY < oppPaddleY + oppPaddleHeight){
+                rallyCount ++;
+                dx = -dx;
+            }
+            else {
+                if(ballY <= 0){
+                    dy = -dy;           
+                } else if(ballY >= canvas.height){
+                    dy = -dy;
+                }
+            }    
+        }
     
-        if(ballX == playerPaddleX && ballY> playerPaddleY  && ballY < playerPaddleY + playerPaddleHeight){
-            dx = -dx
-            rallyCount ++;
+        //check for win or loss
+        if(ballX > canvas.width){
+            winState = true;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            win();
         }
-        if(ballX == oppPaddleX && ballY > oppPaddleY - oppPaddleHeight / 2 && ballY < oppPaddleY + oppPaddleHeight){
-            rallyCount ++;
-            dx = -dx;
+
+        if(ballX < 0){
+            loseState = true;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            lose();
         }
-        if(ballY <= 0){
-            dy = -dy;           
-        } else if(ballY == canvas.height){
-            dy = -dy;
-        }
-        
-        
+
+        oppPaddleY = ballY - (oppPaddleHeight / 2) ;
     }
-
-    oppPaddleY = ballY - (oppPaddleHeight / 2) ;
-
 
 }
 
@@ -120,6 +139,75 @@ function drawBall(){
     ctx.closePath();
 
 }
+
+function win(){
+    ctx.beginPath();
+    ctx.font = '30px Arial';
+    ctx.fillText("You win! Play again?", canvas.width / 2, canvas.height / 2);
+    ctx.textAlign = 'center';
+    ctx.closePath();
+    
+    // let yes = new Path2D;
+    // ctx.beginPath();
+    // ctx.font = '20px Arial';
+    // yes.fillText("Yes")
+    // ctx.closePath();
+
+    // canvas.addEventListener('click', (e) => {
+    //     if(ctx.isPointInPath(yes, e.offsetX, e.offsetY)){
+    //         draw();
+    //     }
+    // })
+
+    ctx.beginPath();
+    ctx.font = '20px Arial';
+    ctx.textAlign = 'center';
+    ctx.fillText("No", canvas.width / 4, canvas.height / 3)
+    ctx.closePath();
+}
+
+function lose(){
+    ctx.beginPath();
+    ctx.font = '30px Arial';
+    ctx.textAlign = 'center';
+    ctx.fillText("You lose! Play again?",  canvas.width / 2, canvas.height / 2);   
+    ctx.closePath();
+
+    //define yes as new path, use this variable for addeventlistener
+    let yes = new Path2D;
+    //define text, then define 'yes' rectangle on same area as text. when yes is clicked, addeventlistener for invicible rect will trigger
+    ctx.beginPath();
+    ctx.font = '20px Arial';
+    ctx.fillText("Yes", canvas.width / 4 , canvas.height - canvas.height / 3)
+
+    ctx.closePath();
+    ctx.beginPath();
+    yes.rect((canvas.width / 4) - 20 , canvas.height - 20 - canvas.height / 3, 40, 30);
+    ctx.closePath();
+
+    //when yes is clicked, start the game again
+    canvas.addEventListener('click', (e) => {
+        console.log(e)
+        if(ctx.isPointInPath(yes, e.offsetX, e.offsetY)){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            loseState = false;
+            start = false;
+            rallyCount = 0;
+            draw();
+
+        }
+    })
+
+    ctx.beginPath();
+    ctx.font = '20px Arial';
+    ctx.textAlign = 'center';
+    ctx.fillText("No",  canvas.width - canvas.width / 4 , canvas.height - canvas.height / 3)
+    ctx.closePath();
+   
+}
+
+//play again event handlers
+
 
 
 //event handlers for key up and down
